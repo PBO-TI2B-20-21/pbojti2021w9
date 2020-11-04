f1e420e9c6218304e10794f8ca41e8b54e97f4d3
aulafaridah <aulafaridah@gmail.com>
2020-11-04 18:00:27 +0700

    JS09_1941720014_AulaFaridatuzZahro'

---
+++ 1941720014_AulaFaridatuzZahro'_JS09/aula/tugas/Manusia.java
@@ -0,0 +1,16 @@
+package aula.tugas;
+
+/**
+ *
+ * @author Aulafz
+ */
+public class Manusia {
+
+    public void bernafas() {
+        System.out.println("Manusia bisa bernafas");
+    }
+
+    public void makan() {
+        System.out.println("Manusia butuh makan");
+    }
+}
