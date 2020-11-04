8daccec0e5c9e004bf4980dab616a21830e7b3ba
gisandaa <aliyagisanda@gmail.com>
2020-11-04 15:10:34 +0700

    JS9-1941720059-Gisanda Aliya

---
+++ jobsheet9/src/TugasOverriding/Manusia.java
@@ -0,0 +1,10 @@
+package TugasOverriding;
+
+public class Manusia {
+    public void bernafas(){
+        System.out.println("Manusia bisa bernafas");
+    }
+    public void makan(){
+        System.out.println("Wajib makan yang cukup");
+    }
+}
